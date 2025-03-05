c78bbb463817b4447b758e09ba72ca4372127e93
giftedpromise <giftedpromise1992@gmail.com>
2025-03-05 13:41:54 +0100

    Initial commit: Set up Django To-Do List app with Swagger documentation

---
+++ todoapp/admin.py
@@ -0,0 +1,6 @@
+
+from django.contrib import admin
+from .models import Task
+
+# Register your Task model
+admin.site.register(Task)
---
+++ todoapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todoapp/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'todoapp/add_task.html', {'form': form})
